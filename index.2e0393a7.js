01e1cab746468005cec5a157d66b8db51d07902a
Dmitry Kalugin <n.demitsuri@gmail.com>
2022-12-10 01:44:34 +0300

    Updates

--- index.2e0393a7.js
+++
@@ -1,8 +0,0 @@
-const grid = document.createElement('div');
-grid.className = 'grid';
-document.body.appendChild(grid);
-document.addEventListener('keydown', (event)=>{
-    if (event.ctrlKey && event.code === 'KeyG') grid.classList.toggle('grid_visible');
-});
-
-//# sourceMappingURL=index.2e0393a7.js.map
--- index.85fe32ac.js
+++ index.85fe32ac.js
@@ -1,2 +1,2 @@
 const grid=document.createElement("div");grid.className="grid",document.body.appendChild(grid),document.addEventListener("keydown",(e=>{e.ctrlKey&&"KeyG"===e.code&&grid.classList.toggle("grid_visible")}));
-//# sourceMappingURL=index.6b3763a9.js.map
+//# sourceMappingURL=index.85fe32ac.js.map
